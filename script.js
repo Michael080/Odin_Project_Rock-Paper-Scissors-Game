9ab23cf2609161618aaddd7b036ef9fb04de059e
Michael Rollin <mrollin808@gmail.com>
2021-03-14 12:49:23 -0600

    Store score, name, weapon, and score-incrementing-method on instantiation of Player object.

--- script.js
+++ script.js
@@ -3,45 +3,81 @@ let weapon = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
+const weaponsMatchUp = {
+  rock: 'scissors',
+  paper: 'rock',
+  scissors: 'paper'
+}
+
+function Player(score, name, weapon) {
+  this.score = score;
+  this.name = name;
+  this.weapon = weapon;
+
+  this.winner = () => this.score ++;
+}
+
+let playerObj = new Player(0, 'Player-1');
+let computerObj = new Player(0, 'HAL');
+
 function cpuWeaponSelect() {
     random_weapon = Math.floor(Math.random() * 3);
     return weapon[random_weapon].toLowerCase();
 }
 
-function scoreBoard(winWeapon, loseWeapon, point, win = false){
-    point++;
-    console.log('');
-    console.log('----------------------------')
-    console.log(`>> ${winWeapon} beats ${loseWeapon}`);
-    console.log(`You: ${playerScore} vs. CPU: ${computerScore}`);
-    win ? console.log('You win!'):
-            console.log('You lose :(');
+function scoreBoard(winWeapon, loseWeapon, draw = false){
+    const winLoseMatch = `>> ${winWeapon.toUpperCase()} beats ${loseWeapon.toUpperCase()}`;
+    const drawMatch = `It's a draw.`
+
+    // Log match results & score
+    console.clear();
+
+    draw ? console.log(drawMatch) : console.log(winLoseMatch);
+    console.log('...');
+    console.log(`You: ${playerObj.score} vs. CPU: ${computerObj.score}`);
+}
+
+// Pass winner obj. & increment score 1 point
+// Pass prop. - currentRound & display corresponding message
+// winFunc(playerObj.score, playerObj.currentRound, 'draw') 
+function winFunc(winnerScore, outcome) {
+  const outcomes = {
+    win: () => {
+      playerObj.winner();
+      scoreBoard(playerObj.weapon, computerObj.weapon);
+    },
+    draw: () => {
+      scoreBoard(playerObj.weapon, computerObj.weapon, true);
+    },
+    lose: () => {
+      computerObj.winner();
+      scoreBoard(computerObj.weapon, playerObj.weapon);
+    }
+  }
+
+  return outcomes[outcome]();
 }
 
 function matchUp(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log('');
-        console.log('----------------------------')
-        console.log('Draw...');
-        console.log(`You: ${playerScore} vs. CPU: ${computerScore}`);
-    } else {
-        switch (true) {
-            case (playerSelection === 'rock' && computerSelection === 'scissors'):
-            case (playerSelection === 'paper' && computerSelection === 'rock'):
-            case (playerSelection === 'scissors' && computerSelection === 'paper'):
-                scoreBoard(playerSelection, computerSelection, playerScore, true);
-                break;
-                default:
-                    scoreBoard(computerSelection, playerSelection, computerScore);
-        }
-    }
+    // Id win-condition for players
+    playerObj.beats = weaponsMatchUp[playerObj.weapon];
+    computerObj.beats = weaponsMatchUp[computerObj.weapon];
+
+    playerObj.beats === computerObj.weapon ?
+      // win condition:
+      winFunc(playerObj.score, 'win') :
+      computerObj.beats === playerObj.weapon ?
+        // lose condition:
+        winFunc(computerObj.score, 'lose') :
+        // draw condition:
+        winFunc(playerObj.score, 'draw'); 
 }
 
 function playRounds(rounds) {
     for (i = 0; i < rounds; i++) {
-        var playerSelection = prompt('Choose your weapon:').toLowerCase();
-        var computerSelection = cpuWeaponSelect();
-        matchUp(playerSelection, computerSelection);
+        playerObj.weapon = prompt('Choose your weapon:').toLowerCase();;
+        computerObj.weapon = cpuWeaponSelect();
+        matchUp(playerObj.weapon, computerObj.weapon);
     }
 }
 
